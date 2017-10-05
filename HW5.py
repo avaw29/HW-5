1288bb870d77e7553771a66cf06af9a4093878e5
Ava Weiner <avaw@umich.edu>
2017-10-05 13:12:12 -0400

    changed variables

--- HW5.py
+++ HW5.py
@@ -2,9 +2,9 @@ import re
 
 fname = "regex_sum_38405.txt"
 f = open(fname, "r")
-numbers = []
+ints = []
 for line in f.readlines():
-    nums = re.findall('[0-9]+', line)
-    for x in nums:
-        numbers.append(int(x))
-print (sum(numbers))
+    numbers = re.findall('[0-9]+', line)
+    for x in numbers:
+        ints.append(int(x))
+print (sum(ints))
